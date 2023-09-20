d84f4b44e4838f1d9071ebc789f52d3634d04b30
fanxinqi <fanxinqi@taou.com>
2023-09-20 16:52:17 +0800

    修改样式

--- src/component/answer.tsx
+++ src/component/answer.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import useEffectOnce from "../utils/useEffectOnce";
 import TextDisplay from "./TextDisplay";
+const iconUrl = "https://i9.taou.com/maimai/p/34715/1782_6_8JfCNQpHxkJxn6";
 
 const Answer = () => {
+  const [showAiBox, setAiBox] = useState(false);
+  const [aiBoxText, setAiBoxText] = useState("打开滴答AI助手");
   useEffectOnce(() => {
     const title = (document.querySelector(
       ".QuestionHeader-title"
@@ -16,20 +19,39 @@ const Answer = () => {
     }
   });
 
+  const toggleAiBox = () => {
+    setAiBox((pre) => !pre);
+    setAiBoxText((pre) => {
+      if (pre === "打开滴答AI助手") {
+        return "关闭滴答AI助手";
+      } else {
+        return "打开滴答AI助手";
+      }
+    });
+  };
+
   return (
     <>
-      <div className="alter-title">
-        <img
-          className="alter-title-icon"
-          src={
-            "https://gips2.baidu.com/it/u=1070828945,3304149431&fm=3028&app=3028&f=PNG&fmt=auto&q=75&size=f150_150"
-          }
-        />
-        <div className="alter-title-text">知乎小助手</div>
+      <div className="alter-ball-box">
+        <div className="alter-ball" onClick={toggleAiBox}>
+          <div className="alter-ball-text">{aiBoxText}</div>
+          {/* <div className="alter-ball-version">gpt4.0</div> */}
+        </div>
       </div>
-      <div className="alter-answer">
-        <TextDisplay />
-      </div>
+      {showAiBox && (
+        <div className="alter-ai-box">
+          <div className="alter-title">
+            <img
+              className="alter-title-icon"
+              src={"https://i9.taou.com/maimai/p/34715/1782_6_8JfCNQpHxkJxn6"}
+            />
+            <div className="alter-title-text">知乎小助手</div>
+          </div>
+          <div className="alter-answer">
+            <TextDisplay />
+          </div>
+        </div>
+      )}
     </>
   );
 };
